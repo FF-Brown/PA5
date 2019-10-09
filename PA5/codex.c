efc12bc692e55b2895d0bf5345bfb70680623e2f
Nathan Brown <nathan.g.brown@wsu.edu>
2019-10-08 20:06:00 -0700

    Add project files.

---
+++ PA5/codex.c
@@ -0,0 +1,7 @@
+/*
+	Author: Nathan Brown
+	Created: 10/08/2019
+	Description: Text-based version of Yahtzee! Function definitions
+*/
+
+#include"codex.h"
---
+++ PA5/main.c
@@ -0,0 +1,7 @@
+/*
+	Author: Nathan Brown
+	Created: 10/08/2019 (National Hydrogen and Fuel Cell Day! Because the atomic weight of hydrogen is 1.008)
+	Description: Text-based version of Yahtzee!
+*/
+
+#include"codex.h"
